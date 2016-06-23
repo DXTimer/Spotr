f9ceacfc3739440ddd005aac96f24404262db1f6
rahulrama <rahulrama@gmail.com>
2016-06-23 08:58:04 +0100

    Travis wip

---
+++ spec/javascripts/karma.conf.js
@@ -0,0 +1,41 @@
+module.exports = function(config){
+  config.set({
+
+    basePath: '../',
+
+    files: [
+      'lib/assets/bower_components/angular/angular.js',
+      'lib/assets/bower_components/angular-mocks/angular-mocks.js',
+      'lib/assets/bower_components/ngmap/build/scripts/ng-map.min.js',
+      'app/assets/javasripts/**/*.js',
+      'app/assets/javasripts/**/**/*.js',
+      'app/assets/javasripts/*.js',
+      'spec/javasripts/unit/**/*.js',
+    ],
+
+    autoWatch: true,
+    frameworks: ['jasmine'],
+    browsers: ['Chrome'],
+    reporters: ['progress', 'coverage', 'coveralls'],
+
+    coverageReporter: {
+      type: 'lcov',
+      dir: 'coverage/'
+    },
+
+    plugins: [
+      'karma-chrome-launcher',
+      'karma-firefox-launcher',
+      'karma-jasmine',
+      'karma-junit-reporter',
+      'karma-phantomjs-launcher',
+      'karma-coverage',
+      'karma-coveralls'
+    ],
+
+    junitReporter: {
+      outputFile: 'test_out/unit.xml',
+      suite: 'unit'
+    }
+  });
+};
---
+++ spec/javascripts/protractor.conf.js
@@ -0,0 +1,29 @@
+exports.config = {
+
+  allScriptsTimeout: 11000,
+
+  specs: [
+    'e2e/*.js'
+  ],
+
+  capabilities: {
+    'browserName': 'chrome'
+  },
+
+  baseUrl: 'http://localhost:8000/',
+
+  framework: 'jasmine',
+
+  jasmineNodeOpts: {
+    defaultTimeoutInterval: 30000
+  },
+
+  onPrepare: function(){
+    require('protractor-http-mock').config = {
+      rootDirectory: process.cwd(),
+      protractorConfig: 'test/protractor.conf.js'
+    };
+  }
+
+};
+
